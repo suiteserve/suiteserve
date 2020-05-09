0381fa744557402b9ad1be27b95c4c9f97cc1458
TJ Mazeika <tj.mazeika@pm.me>
2020-05-09 19:13:09 -0400

    Work on UI for cases

--- public/static/script.js
+++ public/static/script.js
@@ -1,27 +1,57 @@
+let activeSuiteElem;
+
 const app = new Vue({
     el: '#app',
+    created: function () {
+        fetch('/suites')
+            .then(res => res.json()
+                .then(suites => this.suites = suites));
+    },
     data: {
         suites: [],
+        cases: [],
+        runningSuites: 0,
+    },
+    watch: {
+        suites: {
+            deep: true,
+            handler: function (suites) {
+                this.runningSuites = suites
+                    .filter(s => s.status === 'running')
+                    .length;
+            },
+        }
     },
     methods: {
-        openSuite: function(event, suite) {
+        openSuite: function (event, suite) {
+            event.preventDefault();
             const e = event.currentTarget;
+
+            if (activeSuiteElem) {
+                activeSuiteElem.classList.remove('active');
+            }
+            activeSuiteElem = e;
+            activeSuiteElem.classList.add('active');
+
             const statusIcon = e.querySelector('.status-icon');
             const statusIconClasses = statusIcon.classList;
 
             if (statusIconClasses.contains('created')) {
                 statusIconClasses.replace('created', 'running');
+                suite.status = 'running';
             } else if (statusIconClasses.contains('running')) {
                 statusIconClasses.replace('running', 'finished');
+                suite.status = 'finished';
             } else {
                 statusIconClasses.replace('finished', 'running');
+                suite.status = 'running';
             }
 
             fetch(`/suites/${suite.id}/cases`)
                 .then(res => res.json()
-                    .then(cases => suite.cases = cases));
+                    .then(cases => this.cases = cases));
         },
-        formatTime: function(millis) {
+        formatTime: function (millis) {
             const date = new Date(millis);
             const opts = {
                 weekday: 'short',
@@ -36,9 +66,3 @@ const app = new Vue({
         }
     },
 });
-
-(function () {
-    fetch('/suites')
-        .then(res => res.json()
-            .then(suites => app.suites = suites));
-})()
