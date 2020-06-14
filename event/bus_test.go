1b02e135d164c8d11c86a644e42bbb439a9aff7e
TJ Mazeika <tj.mazeika@pm.me>
2020-06-14 14:41:57 -0400

    Add bus_test.go

---
+++ event/bus_test.go
@@ -0,0 +1,23 @@
+package event
+
+import (
+	"sync"
+	"testing"
+)
+
+var wg sync.WaitGroup
+
+func TestBus_Publish(t *testing.T) {
+	var bus Bus
+	sub := bus.Subscribe()
+	defer sub.Unsubscribe()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		bus.Publish("event")
+	}()
+	if e := <-sub.Ch(); e != "event" {
+		t.Errorf("got %q, want %q", e, "event")
+	}
+	wg.Wait()
+}
